4da70a9b62b79c63648d6b46bec63d6afa04b459
felipemartinez-code <fnmartinez@frd.utn.edu.ar>
2025-11-01 23:05:52 -0300

    correciones

---
+++ frontend/src/ProductForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductForm() {
+  return (
+    <div>
+      <h2>Formulario para Crear Producto</h2>
+      <p>Esta página está en construcción.</p>
+    </div>
+  );
+}
+
+export default ProductForm;
